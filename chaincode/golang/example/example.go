c149cdbf0fa83480de502f07f5cf5cc5e77dc645
Percival Lucena <plucena@gmail.com>
2025-02-11 15:17:06 -0300

    Update example.go

--- chaincode/golang/example/example.go
+++ chaincode/golang/example/example.go
@@ -1,205 +1,162 @@
-/*
-Copyright IBM Corp. 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
-//WARNING - this chaincode's ID is hard-coded in chaincode_example04 to illustrate one way of
-//calling chaincode from a chaincode. If this example is modified, chaincode_example04.go has
-//to be modified as well with the new ID of chaincode_example02.
-//chaincode_example05 show's how chaincode ID can be passed in as a parameter instead of
-//hard-coding.
-
 import (
+	"encoding/json"
 	"fmt"
-	"strconv"
 
-	// "github.com/hyperledger/fabric/core/chaincode/shim" 
 	"github.com/hyperledger/fabric-chaincode-go/shim"
-	
-	// pb "github.com/hyperledger/fabric/protos/peer"
-	pb "github.com/hyperledger/fabric-protos-go/peer"
+	"github.com/hyperledger/fabric-protos-go/peer"
 )
 
-// SimpleChaincode example simple Chaincode implementation
-type SimpleChaincode struct {
+// Product represents the structure of a product
+type Product struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
 }
 
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("example Init")
+// SimpleChaincode implements the chaincode interface
+type SimpleChaincode struct{}
 
-	_, args := stub.GetFunctionAndParameters()
-	var A, B string    // Entities
-	var Aval, Bval int // Asset holdings
-	var err error
-
-	if len(args) != 4 {
-		return shim.Error("Incorrect number of arguments. Expecting 4")
-	}
-
-	// Initialize the chaincode
-	A = args[0]
-	Aval, err = strconv.Atoi(args[1])
-	if err != nil {
-		return shim.Error("Expecting integer value for asset holding")
-	}
-	B = args[2]
-	Bval, err = strconv.Atoi(args[3])
-	if err != nil {
-		return shim.Error("Expecting integer value for asset holding")
-	}
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-
-	// Write the state to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	
-
+// Init is called during chaincode instantiation
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) peer.Response {
 	return shim.Success(nil)
 }
 
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("ex02 Invoke")
+// Invoke is called to invoke the chaincode functions
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 	function, args := stub.GetFunctionAndParameters()
-	if function == "invoke" {
-		// Make payment of X units from A to B
-		return t.invoke(stub, args)
-	} else if function == "delete" {
-		// Deletes an entity from its state
-		return t.delete(stub, args)
-	} else if function == "query" {
-		// the old "Query" is now implemtned in invoke
-		return t.query(stub, args)
-	}
 
-	return shim.Error("Invalid invoke function name. Expecting \"invoke\" \"delete\" \"query\"")
+	switch function {
+	case "createProduct":
+		return t.createProduct(stub, args)
+	case "readProduct":
+		return t.readProduct(stub, args)
+	case "updateProduct":
+		return t.updateProduct(stub, args)
+	case "deleteProduct":
+		return t.deleteProduct(stub, args)
+	default:
+		return shim.Error("Invalid function name. Available functions: createProduct, readProduct, updateProduct, deleteProduct")
+	}
 }
 
-// Transaction makes payment of X units from A to B
-func (t *SimpleChaincode) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var A, B string    // Entities
-	var Aval, Bval int // Asset holdings
-	var X int          // Transaction value
-	var err error
-
+// createProduct adds a new product to the ledger
+func (t *SimpleChaincode) createProduct(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	if len(args) != 3 {
-		return shim.Error("Incorrect number of arguments. Expecting 3")
+		return shim.Error("Incorrect number of arguments. Expected: id, name, price")
 	}
 
-	A = args[0]
-	B = args[1]
-
-	// Get the state from the ledger
-	// TODO: will be nice to have a GetAllState call to ledger
-	Avalbytes, err := stub.GetState(A)
+	// Parse the price as an integer
+	price := 0
+	_, err := fmt.Sscanf(args[2], "%d", &price)
 	if err != nil {
-		return shim.Error("Failed to get state")
-	}
-	if Avalbytes == nil {
-		return shim.Error("Entity not found")
-	}
-	Aval, _ = strconv.Atoi(string(Avalbytes))
-
-	Bvalbytes, err := stub.GetState(B)
-	if err != nil {
-		return shim.Error("Failed to get state")
-	}
-	if Bvalbytes == nil {
-		return shim.Error("Entity not found")
-	}
-	Bval, _ = strconv.Atoi(string(Bvalbytes))
-
-	// Perform the execution
-	X, err = strconv.Atoi(args[2])
-	if err != nil {
-		return shim.Error("Invalid transaction amount, expecting a integer value")
-	}
-	Aval = Aval - X
-	Bval = Bval + X
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-
-	// Write the state back to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	if err != nil {
-		return shim.Error(err.Error())
+		return shim.Error("Price must be a number")
 	}
 
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
+	// Create a new product
+	product := Product{
+		ID:    args[0],
+		Name:  args[1],
+		Price: price,
+	}
+
+	// Convert the product to JSON
+	productJSON, err := json.Marshal(product)
 	if err != nil {
-		return shim.Error(err.Error())
+		return shim.Error("Failed to marshal product to JSON")
+	}
+
+	// Save the product to the ledger
+	err = stub.PutState(product.ID, productJSON)
+	if err != nil {
+		return shim.Error("Failed to save product to the ledger")
 	}
 
 	return shim.Success(nil)
 }
 
-// Deletes an entity from state
-func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+// readProduct retrieves a product from the ledger by its ID
+func (t *SimpleChaincode) readProduct(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	if len(args) != 1 {
-		return shim.Error("Incorrect number of arguments. Expecting 1")
+		return shim.Error("Incorrect number of arguments. Expected: id")
 	}
 
-	A := args[0]
-
-	// Delete the key from the state in ledger
-	err := stub.DelState(A)
+	// Retrieve the product from the ledger
+	productJSON, err := stub.GetState(args[0])
 	if err != nil {
-		return shim.Error("Failed to delete state")
+		return shim.Error("Failed to read product from the ledger")
+	}
+	if productJSON == nil {
+		return shim.Error("Product not found")
+	}
+
+	return shim.Success(productJSON)
+}
+
+// updateProduct updates an existing product in the ledger
+func (t *SimpleChaincode) updateProduct(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	if len(args) != 3 {
+		return shim.Error("Incorrect number of arguments. Expected: id, name, price")
+	}
+
+	// Retrieve the existing product
+	productJSON, err := stub.GetState(args[0])
+	if err != nil {
+		return shim.Error("Failed to read product from the ledger")
+	}
+	if productJSON == nil {
+		return shim.Error("Product not found")
+	}
+
+	// Parse the price as an integer
+	price := 0
+	_, err = fmt.Sscanf(args[2], "%d", &price)
+	if err != nil {
+		return shim.Error("Price must be a number")
+	}
+
+	// Update the product
+	product := Product{
+		ID:    args[0],
+		Name:  args[1],
+		Price: price,
+	}
+
+	// Convert the updated product to JSON
+	updatedProductJSON, err := json.Marshal(product)
+	if err != nil {
+		return shim.Error("Failed to marshal updated product to JSON")
+	}
+
+	// Save the updated product to the ledger
+	err = stub.PutState(product.ID, updatedProductJSON)
+	if err != nil {
+		return shim.Error("Failed to update product in the ledger")
 	}
 
 	return shim.Success(nil)
 }
 
-// query callback representing the query of a chaincode
-func (t *SimpleChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var A string // Entities
-	var err error
-
+// deleteProduct removes a product from the ledger by its ID
+func (t *SimpleChaincode) deleteProduct(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	if len(args) != 1 {
-		return shim.Error("Incorrect number of arguments. Expecting name of the person to query")
+		return shim.Error("Incorrect number of arguments. Expected: id")
 	}
 
-	A = args[0]
-
-	// Get the state from the ledger
-	Avalbytes, err := stub.GetState(A)
+	// Delete the product from the ledger
+	err := stub.DelState(args[0])
 	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
-		return shim.Error(jsonResp)
+		return shim.Error("Failed to delete product from the ledger")
 	}
 
-	if Avalbytes == nil {
-		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
-		return shim.Error(jsonResp)
-	}
-
-	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
-	fmt.Printf("Query Response:%s\n", jsonResp)
-	return shim.Success(Avalbytes)
+	return shim.Success(nil)
 }
 
+// main function starts the chaincode
 func main() {
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
+		fmt.Printf("Error starting SimpleChaincode: %s", err)
 	}
 }
